045001f50f738afd3155b1afca329a7f3bd2b437
Joao-Pedro <joaopmalvarez@hotmail.com>
2026-09-17 10:25:29 -0300

    leitura

--- MP4/Cassete.c
+++ MP4/Cassete.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void abreArquivo() {
+    FILE *arquivo = fopen("tape.in", "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    }
+    char linha[100];
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        printf("%s", linha);
+    }
+    fclose(arquivo);
+    return 0;
+}
+
 
 int main() {
+    
 }
