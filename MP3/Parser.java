fa7cec2c51f87013547e6a9bc986c58a6a3a6413
Joao-Pedro <joaopmalvarez@hotmail.com>
2026-09-20 20:38:04 -0300

    pareser

--- MP3/Parser.java
+++ MP3/Parser.java
@@ -5,21 +5,21 @@ import java.util.List;
 public class Parser {
     public static CasoTeste[] traduz(List<String> linhas) {
         int indice = pularLinhasVazias(linhas, 0);
-        int n = Integer.parseInt(linhas.get(indice)); // quantos casos de teste
+        int n = Integer.parseInt(linhas.get(indice).trim()); // quantos casos de teste
         CasoTeste[] casos = new CasoTeste[n];
 
         indice++;
         int t, l, m, s;
         for (int i = 0; i < n; i++) {
-            indice = pularLinhasVazias(linhas, indice); 
-            String[] parte = linhas.get(indice).split(" ");
+            indice = pularLinhasVazias(linhas, indice);
+            String[] parte = linhas.get(indice).trim().split("\\s+");
             l = Integer.parseInt(parte[0]);
             int capacidadeLado = l * 30; // (l * 60) / 2
             t = Integer.parseInt(parte[1]);
             indice++;
             Musica[] musicas = new Musica[t];
-            for(int j = 0; j < t; j++) {
-                parte = linhas.get(indice).split(" "); // substitui as antigas
+            for (int j = 0; j < t; j++) {
+                parte = linhas.get(indice).trim().split("\\s+");
                 m = Integer.parseInt(parte[0]);
                 s = Integer.parseInt(parte[1]);
                 musicas[j] = new Musica(m, s);
@@ -31,10 +31,10 @@ public class Parser {
         return casos;
     }
 
-    public static int pularLinhasVazias(List<String> linhas, int indice) { // verifica e pula linhas em branco
-        while (linhas.get(indice).trim().isEmpty()) {
+    public static int pularLinhasVazias(List<String> linhas, int indice) {
+        while (indice < linhas.size() && linhas.get(indice).trim().isEmpty()) {
             indice++;
         }
-        return indice; 
+        return indice;
     }
 }
