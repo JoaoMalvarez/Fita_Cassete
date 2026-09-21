ed7e27f424d5756da4391575043e4652e7ade231
Joao-Pedro <joaopmalvarez@hotmail.com>
2026-09-20 21:39:43 -0300

    correcao caso T = 50

--- MP3/Mochila.java
+++ MP3/Mochila.java
@@ -7,34 +7,52 @@ public class Mochila {
 
     public static boolean[] resolver(Musica[] musicas, int capacidade) {
         int n = musicas.length;
-
-        melhorSoma = 0;
         melhorEscolha = new boolean[n];
 
-        boolean[] usadaAtual = new boolean[n];
-        backtracking(musicas, capacidade, 0, 0, usadaAtual);
+        Integer[][] memo = new Integer[n][capacidade + 1];
+        melhorSoma = backtracking(musicas, capacidade, 0, 0, memo);
+
+        int indice = 0;
+        int somaAcumulada = 0;
+
+        while (indice < n) {
+            int alvo = backtracking(musicas, capacidade, indice, somaAcumulada, memo);
+            int duracao = musicas[indice].duracaoEmSegundos();
+            int valorSemUsar = backtracking(musicas, capacidade, indice + 1, somaAcumulada, memo);
+
+            if (valorSemUsar == alvo) {
+                melhorEscolha[indice] = false;
+            } else {
+                melhorEscolha[indice] = true;
+                somaAcumulada += duracao;
+            }
+            indice++;
+        }
 
         return melhorEscolha; 
     }
 
-    private static void backtracking(Musica[] musicas, int capacidade, int indice, int somaAtual, boolean[] usadaAtual) {
-
+    private static int backtracking(Musica[] musicas, int capacidade, int indice, int somaAtual, Integer[][] memo) {
         if (indice == musicas.length) {
-            if (somaAtual > melhorSoma) {
-                melhorSoma = somaAtual;
-                System.arraycopy(usadaAtual, 0, melhorEscolha, 0, usadaAtual.length);
-            }
-            return;
+            return somaAtual;
+        }
+
+        if (memo[indice][somaAtual] != null) {
+            return memo[indice][somaAtual];
         }
 
         int duracao = musicas[indice].duracaoEmSegundos();
 
-        backtracking(musicas, capacidade, indice + 1, somaAtual, usadaAtual);
+        int semUsar = backtracking(musicas, capacidade, indice + 1, somaAtual, memo);
 
+
+        int comUsar = 0;
         if (somaAtual + duracao <= capacidade) {
-            usadaAtual[indice] = true;
-            backtracking(musicas, capacidade, indice + 1, somaAtual + duracao, usadaAtual);
-            usadaAtual[indice] = false; 
+            comUsar = backtracking(musicas, capacidade, indice + 1, somaAtual + duracao, memo);
         }
+
+        int melhor = Math.max(semUsar, comUsar);
+        memo[indice][somaAtual] = melhor; 
+        return melhor;
     }
 }
